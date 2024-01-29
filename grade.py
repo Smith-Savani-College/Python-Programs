22f4478969c6834fea20a4b1829d1fd97aea0e7d
Smith-Savani-College <158023591+Smith-Savani-College@users.noreply.github.com>
2024-01-29 12:25:08 +0530

    Create grade.py

---
+++ grade.py
@@ -0,0 +1,14 @@
+x = int(input("Enter your marks:"))
+
+if(x>=90):
+    print("Your grade is O")
+elif(x>=80 and x<90):
+    print("Your grade is A")
+elif(x>=70 and x<80):
+    print("Your grade is B")
+elif(x>=60 and x<70):
+    print("Your grade is C")
+elif(x>=50 and x<60):
+    print("Your grade is D")
+else:
+    print("Your grade is E")
