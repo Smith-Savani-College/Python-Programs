9a525fe7a9b6a168457f7e8217f934aba8326fed
Smith-Savani-College <158023591+Smith-Savani-College@users.noreply.github.com>
2024-01-29 12:25:34 +0530

    Create greatest_number.py

---
+++ greatest_number.py
@@ -0,0 +1,13 @@
+num1 = int(input("Enter number 1:"))
+num2 = int(input("\nEnter number 2:"))
+num3 = int(input("\nEnter number 3:"))
+
+
+if(num1>num2 and num1>num3):
+    print(str(num1) + " is largest")
+elif(num2>num1 and num2>num3):
+    print(str(num2) +" is largest")
+elif(num3>num1 and num3>num2):
+    print(str(num3) +" is largest")
+
+
